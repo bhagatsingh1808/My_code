090db1fed2d302ff686eade5e566ab3b8ebec14f
bhagatsingh1808 <49263286+bhagatsingh1808@users.noreply.github.com>
2020-04-09 17:29:25 +0530

    Add files via upload
    Given a series 7,15,32,67.... Find the nth term.

---
+++ SolveSeries.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class SolveSeries {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int[] arr = new int[s];
+        arr[0]=7;
+        for(int i=1; i<s; i++){
+            arr[i] = (arr[i-1]*2)+i;
+        }
+        System.out.println(arr[s-1]);
+        
+    }
+}

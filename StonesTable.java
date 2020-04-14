157fcf05bbc5202017c1d036b5e8e680c7dc4f7c
bhagatsingh1808 <49263286+bhagatsingh1808@users.noreply.github.com>
2020-04-14 09:04:20 +0530

    Add files via upload

---
+++ StonesTable.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        int T = sc.nextInt();
+         sc.nextLine();
+        int count = 0;
+        String S = sc.nextLine();
+        char[] c =S.toCharArray();
+        for(int i=0; i<c.length-1; i++){
+            if(c[i]==c[i+1]){
+                count = count+1;
+                
+            }
+            
+        }System.out.print(count);
+    }
+}
